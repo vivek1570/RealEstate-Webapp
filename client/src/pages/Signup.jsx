fa99170623f269da09ea5b42eadf79dfc86032c9
vivek1570 <kpvivekvettam@gmail.com>
2024-05-25 01:05:30 +0530

    complete sign-up page UI

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Signup() {
+  const handleclick = () => {
+    console.log("hello");
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
@@ -27,10 +30,13 @@ function Signup() {
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90">
           Submit
         </button>
-        <button className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-90">
+        <button
+          className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-90"
+          onClick={handleclick}
+        >
           Continue With Google
         </button>
-        <div className="flex flex-row gap-2">
+        <div className="flex flex-row gap-2 mt-5">
           <p>Dont have a account?</p>
           <Link to="/sign-in">
             <p className="text-blue-600">Sign up</p>
