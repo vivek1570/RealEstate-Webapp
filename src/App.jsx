30b30a36870cc627c14333cae271a2d8866050f1
vivek1570 <kpvivekvettam@gmail.com>
2024-05-24 18:58:40 +0530

    header update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Signin from "./pages/Signin"
-import Signup from "./pages/Signup"
-import Profile from "./pages/Profile"
-import About from "./pages/About"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+
+import Profile from "./pages/Profile";
+
+import About from "./pages/About";
+import Header from "./components/Header";
 
 function App() {
   return (
-<BrowserRouter>
-    <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/sign-in" element={<Signin/>}/>
-        <Route path="/sign-up" element={<Signup/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/about" element={<About/>}/>
-    </Routes>
-</BrowserRouter>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
