5a7702177de6f636604ccc72894a8e8975e90bd6
vivek1570 <kpvivekvettam@gmail.com>
2024-05-26 17:26:49 +0530

    sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -12,6 +13,27 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return errorHandler("User not found", 404);
+    }
+    console.log(validUser);
+    console.log("valid user password", validUser.password);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return errorHandler("Wrong Credentials", 400);
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res.cookie("token", token, { httpOnly: true }).status(200).json(rest);
   } catch (error) {
     next(error);
   }
