4552a227024431122782bcbf623e9d10d0bfc11a
vivek1570 <kpvivekvettam@gmail.com>
2024-05-31 18:32:49 +0530

    added button toggling

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -30,11 +30,15 @@ function Profile() {
   const [fileperc, setFileperc] = useState(0);
   const [fileuploaderror, setFileuploaderror] = useState(false);
   const [update, setupdate] = useState(false);
-
   const [formdata, setformdata] = useState({});
+  const [showListingError, setshowListingError] = useState(false);
+  const [userListings, setuserListings] = useState([]);
+  const [listbtn, setListbtn] = useState(false);
   const dispatch = useDispatch();
   //   console.log(formdata);
+  //   console.log(userListings);
 
+  console.log(userListings);
   useEffect(() => {
     if (file) handleFileUpload(file);
   }, [file]);
@@ -71,7 +75,6 @@ function Profile() {
       }
     );
   };
-  console.log(formdata);
   const handleChange = (e) => {
     setformdata({ ...formdata, [e.target.id]: e.target.value });
   };
@@ -129,6 +132,20 @@ function Profile() {
       dispatch(signoutUserSuccess(data));
     } catch (error) {
       dispatch(signoutUserSuccess(error.message));
+    }
+  };
+  const handleListing = async () => {
+    try {
+      const res = await fetch(`/api/user/listing/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setshowListingError(true);
+        return;
+      }
+      setListbtn(!listbtn);
+      setuserListings(data);
+    } catch (error) {
+      setshowListingError(true);
     }
   };
 
@@ -213,6 +230,46 @@ function Profile() {
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-green-700 mt-5">{update ? "Profile updated" : ""}</p>
+      <button
+        onClick={handleListing}
+        className="text-green-700 w-full bg-slate-300 rounded-lg p-3 hover:opacity-95 uppercase mt-5"
+      >
+        Show Listing
+      </button>
+      {showListingError && (
+        <p className="text-red-700">Error showing listings</p>
+      )}
+      {listbtn && userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 mt-5 flex justify-between items-center gap-3"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing-cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold hover:undeline truncate flex-1"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className="flex flex-col items-center">
+                <button className="text-red-700 uppercase">Delete</button>
+                <button className="text-green-700 uppercase">Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
