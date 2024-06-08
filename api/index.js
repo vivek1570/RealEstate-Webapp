fdba1b5af9b3a071e2a47c2dcc5e604490b236b9
vivek1570 <kpvivekvettam@gmail.com>
2024-06-08 12:36:40 +0530

    port change

--- api/index.js
+++ api/index.js
@@ -24,7 +24,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   console.log("Server is running on port 3000!!!");
 });
 
